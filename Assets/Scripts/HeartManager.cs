76cfab4b5b3191bbdfd6b59b87cd4554a0543cc4
PavelKhitrov <eternal579@gmail.com>
2021-12-10 15:13:29 +0700

    Alpha version

---
+++ Assets/Scripts/HeartManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeartManager : MonoBehaviour
+{
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite halfHeart;
+    public Sprite emptyHeart;
+    public FloatValue heartContainers;
+    public FloatValue playerCurrentHealth;
+
+    void Start()
+    {
+        InitHearts();
+    }
+
+    public void InitHearts() //ammount of hears (could be more than 3)
+    {
+        for (int i = 0; i < heartContainers.initialValue; i++)
+        {
+            hearts[i].gameObject.SetActive(true);
+            hearts[i].sprite = fullHeart;
+        }
+    }
+
+    public void UpdateHearts()
+    {
+        float tempHealth = playerCurrentHealth.initialValue / 2; //3 hearts is equal to 6 health
+        for (int i = 0; i < heartContainers.initialValue; i++)
+        {
+            if(i <= tempHealth - 1)
+            {
+                hearts[i].sprite = fullHeart; //full
+            }
+            else if(i >= tempHealth)
+            {
+                hearts[i].sprite = emptyHeart; //empty
+            }
+            else
+            {
+                hearts[i].sprite = halfHeart; //half
+            }
+        }
+    }
+}
